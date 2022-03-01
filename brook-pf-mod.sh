acbc3d15666a32b34d4ddd4b8afd397371bd8c37
X-Stellvia <38447741+X-Stellvia@users.noreply.github.com>
2022-03-01 21:49:32 +0800

    Update brook-pf-mod.sh

--- brook-pf-mod.sh
+++ brook-pf-mod.sh
@@ -76,18 +76,7 @@ check_crontab_installed_status(){
 check_pid(){
     PID=$(ps -ef| grep "brook relays"| grep -v grep| grep -v ".sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}')
 }
-check_new_ver(){
-    echo -e "请输入要下载安装的 Brook 版本号 ${Green_font_prefix}[ 格式是日期，例如: v20180909 ]${Font_color_suffix}
-版本列表请去这里获取：${Green_font_prefix}[ https://github.com/txthinking/brook/releases ]${Font_color_suffix}"
-    read -e -p "直接回车即自动获取:" brook_new_ver
-    if [[ -z ${brook_new_ver} ]]; then
-        brook_new_ver=$(wget -qO- https://api.github.com/repos/txthinking/brook/releases| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g')
-        [[ -z ${brook_new_ver} ]] && echo -e "${Error} Brook 最新版本获取失败！" && exit 1
-        echo -e "${Info} 检测到 Brook 最新版本为 [ ${brook_new_ver} ]"
-    else
-        echo -e "${Info} 开始下载 Brook [ ${brook_new_ver} ] 版本！"
-    fi
-}
+
 check_domain_ip_change(){
     Modify_success="0"
     user_all=$(cat ${brook_conf}|sed '/^\s*$/d')
@@ -127,9 +116,9 @@ Download_brook(){
     [[ ! -e ${file} ]] && mkdir ${file}
     cd ${file}
     if [[ ${bit} == "x86_64" ]]; then
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_amd64" && mv brook_linux_amd64 brook
+        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/v20200801/brook
     else
-        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+        wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/v20200801/brook_linux_386"
         mv brook_linux_386 brook
     fi
     [[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && exit 1
@@ -501,8 +490,6 @@ Install_brook(){
     echo -e "${Info} 开始安装/配置 依赖..."
     Installation_dependency
     Install_Tools
-    echo -e "${Info} 开始检测最新版本..."
-    check_new_ver
     echo -e "${Info} 开始下载/安装..."
     Download_brook
     echo -e "${Info} 开始下载/安装 服务脚本(init)..."
